ad1b8d9989af3396b9cf11195c8d9deb66f42369
Nastya <rakina2004@bk.ru>
2023-03-07 01:20:50 +0300

    file

--- zadacha 4 lab 3.py
+++ zadacha 4 lab 3.py
@@ -1,2 +1,13 @@
-f=0
-while f != 3:
+import random
+error_c = 0
+c = 0
+while error_c < 3:
+    a = random.randint(0, 100)
+    b = random.randint(0, 100)
+    if int(input(f"{a} + {b} = ")) == a + b:
+        c += 1
+        print('Правильно!')
+    else:
+        error_c += 1
+        print('Неправильно!')
+print(f"Игра окончена. Правильных ответов {c}")
